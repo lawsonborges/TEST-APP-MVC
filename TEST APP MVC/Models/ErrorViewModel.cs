01d88c1a1ddbc209d1a36a53ab26b3eb937ab893
Lawson Borges <Lawson Borges@DESKTOP-C610B07>
2024-10-20 23:42:25 +0530

    Add project files.

---
+++ TEST APP MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TEST_APP_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
